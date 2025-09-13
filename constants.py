a014228753c89ee3a0df29e57ddcec495c773830
Randnn <79960675+Randnn@users.noreply.github.com>
2025-09-13 10:13:34 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,15 @@
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+PLAYER_RADIUS = 20
+player_turn_speed = 300
+player_speed = 200
+player_shoot_speed = 500
+player_shoot_cooldown = 0.3
+
+shot_radius = 5
---
+++ player.py
@@ -0,0 +1,54 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+from shot import *
+
+timer = 0
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.timer = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def rotate(self, dt): 
+        self.rotation += player_turn_speed * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        self.timer -= dt
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            if self.timer <= 0:
+                self.shoot()
+
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * player_speed * dt
+
+    def shoot(self):
+        shot = Shot(self.position, self.position)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * player_shoot_speed
+        self.timer = player_shoot_cooldown
+        
